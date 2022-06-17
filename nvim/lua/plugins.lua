c8f2099f92e0f7288e439a7fb1df2f0568e91d7e
Cem Geçgel <gecgelcem@outlook.com>
2022-06-17 16:48:43 +0300

    doc: Fix Comment on Gruvbox Contrast for Dark

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -15,7 +15,7 @@ packer.use({ 'morhetz/gruvbox', config = function()
     vim.env.NVIM_TUI_ENABLE_TRUE_COLOR = 1 -- Use 24-bit colors.
     vim.opt.termguicolors = true           -- Use 24-bit colors.
     vim.g.gruvbox_italic = 1               -- Use italics.
-    vim.g.gruvbox_contrast_dark = 'hard'   -- Soften contrast for dark.
+    vim.g.gruvbox_contrast_dark = 'hard'   -- Harden contrast for dark.
     vim.g.gruvbox_contrast_light = 'hard'  -- Harden contrast for light.
     -- Set the color scheme.
     vim.cmd('autocmd vimenter * ++nested colorscheme gruvbox')
