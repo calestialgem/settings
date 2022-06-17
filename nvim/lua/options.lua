796db6802724d407e0d71aa8097b9289c4a5e0a9
Cem Geçgel <gecgelcem@outlook.com>
2022-06-17 16:26:31 +0300

    fix: Moved to Full Lua and Packer

---
+++ nvim/lua/options.lua
@@ -0,0 +1,38 @@
+-- SPDX-FileCopyrightText: (C) 2022 Cem Geçgel <gecgelcem@outlook.com>
+-- SPDX-License-Identifier: GPL-3.0-or-later
+
+vim.opt.browsedir = 'current' -- Browse in working directory.
+vim.opt.cdhome = true         -- No arguments to cd changes directory to home.
+vim.opt.colorcolumn = '+1'    -- Highlight column 1 after the textwidth.
+vim.opt.expandtab = true      -- Use spaces instead of tabs.
+vim.opt.langmap = {           -- Fix Turkish keyboard.
+    'ğ[',
+    'ü]',
+    'ö{',
+    'ç}' }
+vim.opt.list = true           -- Draw whitespace.
+vim.opt.listchars = {         -- Set characters for drawing whitespace.
+    tab = '»■',
+    trail = '■',
+    extends = '⟩',
+    precedes = '⟨',
+    nbsp = '■' }
+vim.opt.nrformats = {         -- Set characters for increment and decrement.
+    'alpha',
+    'octal',
+    'hex',
+    'bin' }
+vim.opt.number = true         -- Show line numbers.
+vim.opt.path:append('**')     -- Search for files recursively.
+vim.opt.relativenumber = true -- Show line numbers relatively.
+vim.opt.scrolloff = 8         -- Leave space vertically around cursor.
+vim.opt.shiftround = true     -- Indent multiples of tab width.
+vim.opt.shiftwidth = 4        -- Set the tab width.
+vim.opt.sidescrolloff = 8     -- Leave space horizontally around cursor.
+vim.opt.signcolumn = 'number' -- Show signs next to line numbers.
+vim.opt.splitbelow = true     -- Put new windows below.
+vim.opt.splitright = true     -- Put new windows right.
+vim.opt.tabstop = 4           -- Set tab alignment length.
+vim.opt.textwidth = 80        -- Limit text width.
+vim.opt.tildeop = true        -- Make ~ behave like an operator.
+vim.opt.wrap = false          -- Disable wrapping.
---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,27 @@
+-- SPDX-FileCopyrightText: (C) 2022 Cem Geçgel <gecgelcem@outlook.com>
+-- SPDX-License-Identifier: GPL-3.0-or-later
+
+-- Packer as package manager.
+local packer = require('packer')
+packer.init()
+packer.reset()
+packer.use('wbthomason/packer.nvim')
+
+-- Fugitive for git integration.
+packer.use('tpope/vim-fugitive')
+
+-- Gruvbox as theme.
+packer.use({ 'morhetz/gruvbox', config = function()
+    vim.env.NVIM_TUI_ENABLE_TRUE_COLOR = 1 -- Use 24-bit colors.
+    vim.opt.termguicolors = true           -- Use 24-bit colors.
+    vim.g.gruvbox_italic = 1               -- Use italics.
+    vim.g.gruvbox_contrast_dark = 'hard'   -- Soften contrast for dark.
+    vim.g.gruvbox_contrast_light = 'hard'  -- Harden contrast for light.
+    -- Set the color scheme.
+    vim.cmd('autocmd vimenter * ++nested colorscheme gruvbox')
+end })
+
+-- Lightline as status line.
+packer.use({ 'itchyny/lightline.vim', config = function()
+    vim.opt.showmode = false -- Do not show the mode below the status line.
+end })
